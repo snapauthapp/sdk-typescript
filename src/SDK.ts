09cc98faae2fffc6bdda42d9951fb45f1c2285bd
Eric Stern <eric@ericstern.com>
2023-12-06 20:32:13 -0800

    start to shore up responses and avoid leaking throws

--- src/SDK.ts
+++ src/SDK.ts
@@ -18,18 +18,20 @@ import {
  */
 // success:true, token: string
 // success:false, error: info?
-export type AuthResponse = {
-  ok: true
-  token: string
-} | {
-  ok: false
-}
-export type RegisterResponse = {
-  ok: true
-  token: string
-} | {
-  ok: false
-}
+type Result<T, E> =
+  | { ok: true, data: T }
+  | { ok: false, error: E, more?: unknown }
+
+type WebAuthnError =
+  | 'network_failed' // timeouts?
+  | 'bad_request'
+  | 'server_error'
+  | 'canceled_by_user'
+  // Other = 'other',
+  | 'tbd'
+
+export type AuthResponse = Result<{ token: string }, WebAuthnError>
+export type RegisterResponse = Result<{ token: string }, WebAuthnError>
 
 type UserIdOrHandle =
   | { id: string }
@@ -48,6 +50,8 @@ class SDK {
   constructor(publicKey: string, host: string = 'https://api.webauthn.biz') {
     this.apiKey = publicKey
     this.host = host
+    // this.host = 'http://localhost:456'
+
   }
 
   get isWebAuthnAvailable() {
@@ -62,16 +66,22 @@ class SDK {
 
   async startAuth(user: UserIdOrHandle): Promise<AuthResponse> {
     this.requireWebAuthn()
-    const res = await this.api('/auth/createOptions', { user })
-    const options = parseRequestOptions(res)
+    const res = await this.api('/auth/createOptions', { user }) as Result<CredentialRequestOptionsJSON, WebAuthnError>
+    if (!res.ok) {
+      return res
+    }
+    const options = parseRequestOptions(res.data)
     return await this.doAuth(options, user)
   }
 
   async startRegister(user: UserRegistrationInfo): Promise<RegisterResponse> {
     try {
       this.requireWebAuthn()
-      const res = await this.api('/registration/createOptions', { user })
-      const options = parseCreateOptions(res)
+      const res = await this.api('/registration/createOptions', { user }) as Result<CredentialCreationOptionsJSON, WebAuthnError>
+      if (!res.ok) {
+        return res
+      }
+      const options = parseCreateOptions(res.data)
       console.debug(options)
       const credential = await navigator.credentials.create(options)
       console.debug(credential)
@@ -85,14 +95,12 @@ class SDK {
       //user.handle = this._toBase64Url(res.publicKey.user.id)
 
       // @ts-ignore
-      const data = await this.api('/registration/process', { credential: json, user })
-      return {
-        ok: true,
-        ...data,
-      }
+      const response = await this.api('/registration/process', { credential: json, user }) as RegisterResponse
+      return response
     } catch (error) {
       console.error(error)
-      return { ok: false }
+      // @ts-ignore
+      return { ok: false, error: 'tbd', more: [error.name, error.message] }
     }
   }
 
@@ -111,8 +119,12 @@ class SDK {
     // try/catch?
     // const token = await this.startAuth(user)
     // merge w/ startAith?
-    const res = await this.api('/auth/createOptions', {})
-    const options = parseRequestOptions(res)
+    const res = await this.api('/auth/createOptions', {}) as Result<CredentialRequestOptionsJSON, WebAuthnError>
+    if (!res.ok) {
+      // FIXME: not this?
+      return
+    }
+    const options = parseRequestOptions(res.data)
     const response = await this.doAuth(options, undefined)
     callback(response)
   }
@@ -129,7 +141,13 @@ class SDK {
     } catch (error) {
       // welp, problem. ok. what's the error handling story here?
       console.error(error)
-      return { ok: false }
+      // @ts-ignore
+      // NotAllowedError = canceled by user OR webauthn timeout exceeded
+      // Safari:
+      // error.message = "This request has been cancelled by the user."
+      // ^ "Operation timed out."
+      // Firefox: "The request is not allowed by the user agent or the platform in the current context, possibly because the user denied permission."
+      return { ok: false, error: 'tbd', more: [error.name, error.message] }
     }
   }
 
@@ -140,14 +158,12 @@ class SDK {
     // technically the remote server could look up by credential id, but that's a bad idea.
 
     // @ts-ignore
-    const data = await this.api('/auth/process', { credential: json, user })
-    return {
-      ok: true, // FIXME: look for errors,
-      ...data,
-    }
+    const response = await this.api('/auth/process', { credential: json, user })
+
+    return response
   }
 
-  private async api(path: string, body: JsonEncodable) {
+  private async api(path: string, body: JsonEncodable): Promise<Result<any, WebAuthnError>> {
     const headers = new Headers({
       Accept: 'application/json',
       'Content-type': 'application/json',
@@ -158,19 +174,39 @@ class SDK {
       body: JSON.stringify(body),
       headers,
       method: 'POST',
+      signal: AbortSignal.timeout(50),
     })
-    // todo: timeouts?
-    const response = await fetch(request)
-    if (!response.ok) {
-      console.error(request, response)
-      return // return WHAT?
+    // TODO: timeouts?
+    try {
+      const response = await fetch(request)
+      if (!response.ok) {
+        return {
+          ok: false,
+          error: (response.status >= 500 ? 'server_error' : 'bad_request'),
+          more: [response.status, response.statusText],
+        }
+      }
+      return { ok: true, data: await response.json() }
+    } catch (error) {
+      // error.name, error.message, cause
+      // TypeError, "Failed to fetch", bad destination edge
+      // TypeError, "Load failed", bad destination safar
+      // AbortError, "Fetch is aborted", timeout safari
+      // AbortError, "The user aborted a request", timeout edge
+      // TimeoutError,, "The operation timed out.", timeout FF
+      return {
+        ok: false,
+        error: 'network_failed',
+        more: {
+        // @ts-ignore
+          name: error.name,
+        // @ts-ignore
+          message: error.message,
+          te: error instanceof TypeError,
+          ue: error instanceof URIError,
+        },
+      }
     }
-    // switch on status codes etc
-    // expect json
-    const parsedResponse = await response.json()
-    // format wrapping?
-    return parsedResponse
-
   }
 
   /**
