9fa1ff15bcd93235a6095e6c9f929495a2522408
Eric Stern <eric@ericstern.com>
2024-01-31 22:06:17 -0800

    output format

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    library: 'WebAuthnBiz',
+    libraryTarget: 'umd',
+
   },
   module: {
     rules: [
