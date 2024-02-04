9c12038af7e0d2553c9eec320800df70d11bc140
Eric Stern <eric@ericstern.com>
2024-02-04 10:52:54 -0800

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
-    library: 'WebAuthnBiz',
+    library: 'SnapAuth',
     libraryTarget: 'umd',
   },
   module: {
